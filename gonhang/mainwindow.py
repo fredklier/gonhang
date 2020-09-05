8d2f7725852dd1d048d8d1c57b104f7f2f162c15
Fred Lins <fredcox@gmail.com>
2020-09-05 20:56:19 -0300

    add wizard.py :rocket: :rocket: :rocket: 🇷🇴

--- gonhang/mainwindow.py
+++ gonhang/mainwindow.py
@@ -1,25 +1,25 @@
-import coloredlogs
+import sys
 import logging
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 import subprocess
 from gonhang.api import StringUtil
-import time
 
 
 class MainWindow(QtWidgets.QMainWindow):
     logger = logging.getLogger(__name__)
     wmctrlBin = subprocess.getoutput('which wmctrl')
     myCurrentId = ''
+    # -------------------------------------------------------------
+    # Window Flags
+    flags = QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.Tool
+    # -------------------------------------------------------------
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        # coloredlogs.install()
         self.logger.info('Start MainWindow')
         self.setWindowTitle(StringUtil.getRandomString(30))
-
-        # time.sleep(5)
-        # self.myCurrentId = self.getWindowCurrentId(self.windowTitle())
-        # self.logger.info(f'My Current Id: [{self.myCurrentId}]')
+        self.setWindowFlags(self.flags)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
     def getWindowCurrentId(self, windowTitle):
         self.logger.info(f'wmctrl binary found in : {self.wmctrlBin}')
@@ -37,4 +37,11 @@ class MainWindow(QtWidgets.QMainWindow):
         # wmctrl -i -r 0x07a00006 -b add,sticky
         cmd = f'{self.wmctrlBin} -i -r {self.myCurrentId} -b add,sticky'
         output = subprocess.getoutput(cmd)
-        self.logger.info(f'Output from command [{cmd}] is: {output}')
+
+    def contextMenuEvent(self, event: QtGui.QContextMenuEvent):
+        contextMenu = QtWidgets.QMenu(self)
+        configAction = contextMenu.addAction('&Config')
+        quitAction = contextMenu.addAction('&Quit')
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == quitAction:
+            sys.exit()
