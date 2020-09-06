35f57a7582cc2fb95d6fd94c3c78d7636b32c832
Fred Lins <fredcox@gmail.com>
2020-09-06 02:13:21 -0300

    Tomorrow i will continue. sleep now :rocket:

--- gonhang/threads.py
+++ gonhang/threads.py
@@ -1,23 +1,22 @@
 from PyQt5 import QtCore
 import logging
+from gonhang.core import System
 
 
 # thread fast
 class ThreadSystem(QtCore.QThread):
     logger = logging.getLogger(__name__)
     signal = QtCore.pyqtSignal(dict, name='ThreadFastFinish')
+    system = System()
 
     def __init__(self, parent=None):
         super(ThreadSystem, self).__init__(parent)
         self.finished.connect(self.threadFinished)
 
     def threadFinished(self):
-        self.logger.info('Stop ThreadSystem')
         self.start()
 
     def run(self):
-        # self.signal.emit(self.system.getMessage())
-        self.logger.info('Running ThreadSystem')
-        self.msleep(1500)  # sleep for 500ms
-
+        self.signal.emit(self.system.getMessage())
+        self.msleep(500)  # sleep for 500ms
 
