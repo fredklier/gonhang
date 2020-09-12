8d27c3d40c8b8482db28a6c1325ed9472875a1e6
Fred Lins <fredcox@gmail.com>
2020-09-12 11:26:22 -0300

    teste.py

---
+++ teste.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def runcommand (cmd):
+    proc = subprocess.Popen(cmd,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True)
+    std_out, std_err = proc.communicate()
+    return proc.returncode, std_out, std_err
+
+
+print(runcommand('which nvidia-smi'))
--- updateversions.py
+++ updateversions.py
@@ -17,7 +17,7 @@ def updateFile(fileToUpdate, regPattern, newString):
             f.write(line)
 
 
-newVersion = '0.1.1'
+newVersion = '0.1.2'
 pattern = "([0-9].[0-9].[0-9])"
 
 files = ['setup.py', 'gonhang/version.py', 'aur/PKGBUILD']
